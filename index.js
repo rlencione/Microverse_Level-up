2d98435809cfedbc6c7701f0fdee09894e2042ee
rogar <92257405+rlencione@users.noreply.github.com>
2022-12-02 15:05:07 +0000

    Microverse level #1 completed

---
+++ index.js
@@ -0,0 +1,143 @@
+/*Level #1 Video #4 - Variables
+
+let username = "Oscar";
+console.log("Helo", username, "how are you?");
+
+let totalapple = 20;
+let totalUser = 5;
+
+let applesperuser = totalapple / totalUser;
+console.log("You have", applesperuser, "apples at this moment");
+____________________________________________________________________________
+Level #1 Video #5 - Declarating variables JS
+
+let country = "Brasil";
+console.log(country);
+console.log("country");
+
+let myName = "Roberto Garcia";
+console.log("My name is", myName);
+____________________________________________________________________________
+Level #1 Video #7 - Data types
+
+let string = "Brasil";
+let interger = 2022;
+let bol = true;
+
+console.log(string, interger, bol);
+____________________________________________________________________________
+Level #1 Video # - Underfined and null
+
+undefined - a variable has been declared but has not been assigned;
+underfine is false;
+Data type - underfined;
+console.log(typeof undefined);
+var n;
+let p;
+console.log(n, p);
+
+in arithmetic undefined will result a null
+
+var a = 100;
+var b = 15;
+var c;
+
+function under(){
+  var ex = (a + b + c);
+
+  return ex
+}
+console.log(under());
+
+/*null - is an object - can be assigned to a variable as a representatio value; 
+null is false;
+Data type - object 
+
+console.log(typeof null);
+var m = null;
+let o = null;
+console.log(m, o);
+
+var a = 100;
+var b = 15;
+var c = null;
+
+function nu(){
+  var ex = (a + b + c);
+
+  return ex
+}
+console.log(nu());
+
+var t, a = 100;
+
+if (t) {
+  console.log("t is defined");
+} else if (a) {
+  console.log("a is defined");
+} else {
+  console.log("both t and a are not defined");
+} 
+____________________________________________________________________________
+Level #1 Video #10 - Moidifying variables
+
+let myName = "Roberto";
+myName = "Johnson";
+lastName = "Siqueira";
+fullName = myName + lastName;
+
+console.log("Meu nome inteiro eh" + " " + fullName);
+
+let myNumber = 5;
+//myNumber = myNumber + 5;   or
+myNumber += 15;
+console.log(myNumber)
+____________________________________________________________________________
+Level #1 Video #12 - Final assesment 
+//Ex01
+var a;
+a = 7;
+console.log(a);
+
+//Ex02
+var a;
+a = 7;
+var b;
+b = a;
+console.log(b);
+______________________________________________________________________________
+Level #2 Video # -... */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
